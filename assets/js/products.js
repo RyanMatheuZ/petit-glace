a5f87602b3106f02b8d75cb599574480706f0b4c
Ryan Oliveira <oryan424@gmail.com>
2021-08-30 21:26:48 -0300

    PRODUCT page development

---
+++ assets/js/products.js
@@ -0,0 +1,64 @@
+const allProducts = [
+    {
+        id: document.getElementById('chocos-cream'),
+        origin: 'animal',
+        price: 1.99
+    },
+    {
+        id: document.getElementById('sugary-dream'),
+        origin: 'animal',
+        price: 7.50
+    },
+    {
+        id: document.getElementById('green-valley'),
+        origin: 'vegan',
+        price: 4.75
+    },
+    {
+        id: document.getElementById('ice-tower'),
+        origin: 'vegan',
+        price: 6.00
+    },
+    {
+        id: document.getElementById('sweet-beard'),
+        origin: 'animal',
+        price: 3.50
+    },
+    {
+        id: document.getElementById('double-color'),
+        origin: 'animal',
+        price: 3.25
+    },
+    {
+        id: document.getElementById('milk-straw'),
+        origin: 'animal',
+        price: 5.50
+    },
+    {
+        id: document.getElementById('candy-crown'),
+        origin: 'vegan',
+        price: 7.00
+    },
+    {
+        id: document.getElementById('simple-creamy'),
+        origin: 'vegan',
+        price: 2.75
+    },
+    {
+        id: document.getElementById('ovomaltine'),
+        origin: 'animal',
+        price: 6.90
+    },
+    {
+        id: document.getElementById('petit-cream'),
+        origin: 'animal',
+        price: 9.00
+    },
+    {
+        id: document.getElementById('frozen-coffee'),
+        origin: 'vegan',
+        price: 8.00
+    }
+]
+
+export { allProducts }
---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+import { allProducts } from './products'
